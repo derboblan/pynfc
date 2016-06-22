c217151ac41a5cbd60f299eab77d9dedd6bc1a09
Loy van Beek <l.vanbeek@ultimaker.com>
2016-06-22 14:07:14 +0200

    Set correct datafiles in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     version = "1.7.0_alpha1",
     description = "Python bindings for libnfc",
     author = "Mike Auty",
-    data_files = [('examples', ['mifareauth.py'])],
+    data_files = [('examples', ['src/mifareauth.py', 'src/ntag_read.py'])],
     license = "GPL-2",
     py_modules = ['src.pynfc']
 )
